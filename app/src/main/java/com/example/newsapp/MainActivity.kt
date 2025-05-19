33df8ad61db461f3470d5dfc42050510f0e37b33
Mahmud Guseynov <mahmudlmax24@gmail.com>
2025-05-19 19:41:42 +0300

    init commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NewsAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WebViewExample() {
+    // Declare a string that contains a url
+    val mUrl = "https://www.wikipedia.com"
+
+    // Adding a WebView inside AndroidView with layout as full screen
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
+            loadUrl(mUrl)
+        }
+    }, update = {
+        it.loadUrl(mUrl)
+    })
+}
---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.kotlin.serialization)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.hilt.android.gradle.plugin)
+
+    }
+}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.dagger.hilt) apply false
+    alias(libs.plugins.androidx.room) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kapt) apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
